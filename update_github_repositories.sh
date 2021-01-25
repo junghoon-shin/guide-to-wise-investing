77ecf0b9e2894e72431707e48414488697bfcd12
Junghoon Shin <junghoonshin@kaist.ac.kr>
2021-01-26 03:06:42 +0900

    Regular update

--- update_github_repositories.sh
+++ update_github_repositories.sh
@@ -10,6 +10,8 @@ git push -u origin master
 
 github_pages_repository_directory="$current_directory"/../junghoon-shin.github.io
 
+cp -r "$current_directory"/_book/* "$github_pages_repository_directory"
+
 cd "$github_pages_repository_directory"
 
 git add --all
