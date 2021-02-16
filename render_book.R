4ea918be661fc8bcbed6a16668da50b880bd5d0a
Junghoon Shin <junghoonshin@kaist.ac.kr>
2021-02-16 15:51:41 +0900

    Regular update

--- render_book.R
+++ render_book.R
@@ -1,5 +1,11 @@
 #!/usr/bin/Rscript
 
 library(bookdown)
+library(funr)
+library(tidyverse)
+
+project_directory = sys.script() %>% str_extract("^.+(?=/render_book)")
+
+setwd(project_directory)
 
 render_book("index.Rmd")
